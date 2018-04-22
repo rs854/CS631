e9eb638000589d42b38b614358cc823f3065f75d
root <root@ip-172-31-28-183.us-east-2.compute.internal>
2018-04-22 02:26:50 +0000

    Employee list and add new employee

--- njitFitClub.py
+++ njitFitClub.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 from flaskext.mysql import MySQL
 import flask
@@ -13,7 +13,7 @@ mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'user'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'n17s21o1'
-app.config['MYSQL_DATABASE_DB'] = 'nma'
+app.config['MYSQL_DATABASE_DB'] = 'NJITFitnessClub'
 app.config['MYSQL_DATABASE_HOST'] = 'nma.cqcaflzcpcei.us-east-2.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_PORT'] = 33306
 mysql.init_app(app)
@@ -28,10 +28,46 @@ def index():
 
 @app.route("/payroll")
 def payroll():
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from Instructor")
-    r = [dict((re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', cursor.description[i][0]), value) for i, value in enumerate(row)) for row in cursor.fetchall()]
+    cnx = mysql.connect()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM NJITFitnessClub.Instructor")
+    r = [dict((cursor.description[i][0], value) for i, value in enumerate(row)) for row in cursor.fetchall()]
+    cursor.close()
+    cnx.close()
     return render_template('payroll.html', instructors=r)
+
+
+@app.route("/employees", methods=["GET", "POST"])
+def new_employee():
+    if request.method == "GET":
+        return app.send_static_file('new_employee_form.html')
+
+    cnx = mysql.connect()
+    cursor = cnx.cursor()
+
+    name = request.form["Name"]
+    salary = request.form["Salary"]
+    wage = request.form["Wage"]
+    numberOfHoursTaught = request.form["NumberHoursTaught"]
+
+    if salary == "":
+        salary = "NULL"
+
+    if wage == "":
+        wage = "NULL"
+
+    if numberOfHoursTaught == "":
+        numberOfHoursTaught = "NULL"
+
+    query = "INSERT INTO `Instructor` (`Name`, `Salary`, `Wage`, `NumberHoursTaught`) VALUES ('{}', {}, {}, {});".format(name, salary, wage, numberOfHoursTaught)
+    logging.info(query)
+
+    cursor.execute(query)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+    return "Employee Added"
 
 
 def json_serial(obj):
